5fdfb5a7ce043f6a156711680d6b71e8ad7d375e
Apat1chn1y <gordeevdimon@mail.ru>
2021-06-22 02:08:02 +0300

    GADSTVO

--- routes/auth.js
+++ routes/auth.js
@@ -28,7 +28,8 @@ async function checkSignature(data) {
   data_check_string = newDataStr;
   secret_key = sha256(TOKEN)
   let shmack = sha256.hmac(data_check_string, secret_key);
-  if ((shmack.hex()) == data.hash){
+  let nshmack = hex(shmack);
+  if (nshmack == data.hash){
     console.log('true takoy true')
     return true
   }else{
